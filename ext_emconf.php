c83730cde26b3067332ef9705db56768f8822d75
kasper <kasper@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-11-07 12:28:10 +0000

    *** empty log message ***
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tt_guest/trunk@2702 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,9 +1,9 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: 'tt_guest'
+# Extension Manager/Repository config file for ext: "tt_guest"
 # 
-# Auto generated 22-10-2003 23:21
+# Auto generated 07-10-2005 16:00
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -17,6 +17,9 @@ $EM_CONF[$_EXTKEY] = Array (
 	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '-',
+	'PHP_version' => '-',
 	'module' => '',
 	'state' => 'stable',
 	'internal' => 0,
@@ -28,10 +31,12 @@ $EM_CONF[$_EXTKEY] = Array (
 	'author' => 'Kasper Skårhøj',
 	'author_email' => 'kasperYYYY@typo3.com',
 	'author_company' => 'Curby Soft Multimedia',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '1.0.8',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:20:{s:28:"class.tx_ttguest_wizicon.php";s:4:"4122";s:12:"ext_icon.gif";s:4:"6754";s:15:"ext_icon__h.gif";s:4:"e999";s:14:"ext_tables.php";s:4:"3ee0";s:14:"ext_tables.sql";s:4:"4cb5";s:28:"ext_typoscript_constants.txt";s:4:"a432";s:24:"ext_typoscript_setup.txt";s:4:"5335";s:13:"guestbook.gif";s:4:"b513";s:13:"locallang.php";s:4:"cffa";s:25:"locallang_csh_ttguest.php";s:4:"f586";s:17:"locallang_tca.php";s:4:"cb60";s:7:"tca.php";s:4:"9066";s:23:"pi/class.tx_ttguest.php";s:4:"dfd1";s:18:"pi/guest_help.tmpl";s:4:"a7e0";s:18:"pi/guest_help1.gif";s:4:"817b";s:19:"pi/guest_submit.inc";s:4:"a038";s:23:"pi/guest_template1.tmpl";s:4:"e10f";s:23:"pi/guest_template2.tmpl";s:4:"ed22";s:12:"doc/TODO.txt";s:4:"d602";s:14:"doc/manual.sxw";s:4:"a2c1";}',
+	'version' => '1.0.9',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:31:{s:28:"class.tx_ttguest_wizicon.php";s:4:"7963";s:12:"ext_icon.gif";s:4:"6754";s:15:"ext_icon__h.gif";s:4:"e999";s:14:"ext_tables.php";s:4:"3ee0";s:14:"ext_tables.sql";s:4:"4cb5";s:28:"ext_typoscript_constants.txt";s:4:"a432";s:24:"ext_typoscript_setup.txt";s:4:"f8de";s:13:"guestbook.gif";s:4:"b513";s:13:"locallang.php";s:4:"7a32";s:25:"locallang_csh_ttguest.php";s:4:"f586";s:17:"locallang_tca.php";s:4:"a5b5";s:7:"tca.php";s:4:"9066";s:11:"CVS/Entries";s:4:"e9ba";s:14:"CVS/Repository";s:4:"5690";s:8:"CVS/Root";s:4:"bbeb";s:12:"doc/TODO.txt";s:4:"d602";s:14:"doc/manual.sxw";s:4:"744e";s:15:"doc/CVS/Entries";s:4:"2c46";s:18:"doc/CVS/Repository";s:4:"66d2";s:12:"doc/CVS/Root";s:4:"bbeb";s:29:"pi/.#class.tx_ttguest.php.1.2";s:4:"8791";s:23:"pi/class.tx_ttguest.php";s:4:"3bf4";s:39:"pi/class.tx_ttguest_RecordNavigator.php";s:4:"582a";s:18:"pi/guest_help.tmpl";s:4:"a7e0";s:18:"pi/guest_help1.gif";s:4:"817b";s:19:"pi/guest_submit.inc";s:4:"ba89";s:23:"pi/guest_template1.tmpl";s:4:"29f4";s:23:"pi/guest_template2.tmpl";s:4:"dedf";s:14:"pi/CVS/Entries";s:4:"bcd0";s:17:"pi/CVS/Repository";s:4:"169f";s:11:"pi/CVS/Root";s:4:"bbeb";}',
 );
 
 ?>

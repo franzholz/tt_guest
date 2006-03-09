cb39b722640a6f23efdb9fa0270a392231edfef5
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-03-09 08:50:21 +0000

    version 1.0.10
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tt_guest/trunk@2704 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,14 +2,15 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "tt_guest"
-# 
-# Auto generated 07-10-2005 16:00
-# 
+#
+# Auto generated 09-03-2006 09:49
+#
 # Manual updates:
-# Only the data in the array - anything else is removed by next write
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = Array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'Guestbook',
 	'description' => 'Simple guestbook with subject, comment, name, email and www.',
 	'category' => 'plugin',
@@ -18,8 +19,6 @@ $EM_CONF[$_EXTKEY] = Array (
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '-',
-	'PHP_version' => '-',
 	'module' => '',
 	'state' => 'stable',
 	'internal' => 0,
@@ -33,10 +32,19 @@ $EM_CONF[$_EXTKEY] = Array (
 	'author_company' => 'Freelancer',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'private' => 0,
-	'download_password' => '',
-	'version' => '1.0.9',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:31:{s:28:"class.tx_ttguest_wizicon.php";s:4:"7963";s:12:"ext_icon.gif";s:4:"6754";s:15:"ext_icon__h.gif";s:4:"e999";s:14:"ext_tables.php";s:4:"3ee0";s:14:"ext_tables.sql";s:4:"4cb5";s:28:"ext_typoscript_constants.txt";s:4:"a432";s:24:"ext_typoscript_setup.txt";s:4:"f8de";s:13:"guestbook.gif";s:4:"b513";s:13:"locallang.php";s:4:"7a32";s:25:"locallang_csh_ttguest.php";s:4:"f586";s:17:"locallang_tca.php";s:4:"a5b5";s:7:"tca.php";s:4:"9066";s:11:"CVS/Entries";s:4:"e9ba";s:14:"CVS/Repository";s:4:"5690";s:8:"CVS/Root";s:4:"bbeb";s:12:"doc/TODO.txt";s:4:"d602";s:14:"doc/manual.sxw";s:4:"744e";s:15:"doc/CVS/Entries";s:4:"2c46";s:18:"doc/CVS/Repository";s:4:"66d2";s:12:"doc/CVS/Root";s:4:"bbeb";s:29:"pi/.#class.tx_ttguest.php.1.2";s:4:"8791";s:23:"pi/class.tx_ttguest.php";s:4:"3bf4";s:39:"pi/class.tx_ttguest_RecordNavigator.php";s:4:"582a";s:18:"pi/guest_help.tmpl";s:4:"a7e0";s:18:"pi/guest_help1.gif";s:4:"817b";s:19:"pi/guest_submit.inc";s:4:"ba89";s:23:"pi/guest_template1.tmpl";s:4:"29f4";s:23:"pi/guest_template2.tmpl";s:4:"dedf";s:14:"pi/CVS/Entries";s:4:"bcd0";s:17:"pi/CVS/Repository";s:4:"169f";s:11:"pi/CVS/Root";s:4:"bbeb";}',
+	'version' => '1.0.10',
+	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"a11b";s:28:"class.tx_ttguest_wizicon.php";s:4:"7963";s:12:"ext_icon.gif";s:4:"6754";s:15:"ext_icon__h.gif";s:4:"e999";s:14:"ext_tables.php";s:4:"3ee0";s:14:"ext_tables.sql";s:4:"4cb5";s:28:"ext_typoscript_constants.txt";s:4:"a432";s:24:"ext_typoscript_setup.txt";s:4:"1451";s:13:"guestbook.gif";s:4:"b513";s:13:"locallang.php";s:4:"7a32";s:25:"locallang_csh_ttguest.php";s:4:"f586";s:17:"locallang_tca.php";s:4:"a5b5";s:7:"tca.php";s:4:"9066";s:12:"doc/TODO.txt";s:4:"d602";s:14:"doc/manual.sxw";s:4:"744e";s:23:"pi/class.tx_ttguest.php";s:4:"f4ff";s:39:"pi/class.tx_ttguest_RecordNavigator.php";s:4:"582a";s:18:"pi/guest_help.tmpl";s:4:"a7e0";s:18:"pi/guest_help1.gif";s:4:"817b";s:19:"pi/guest_submit.inc";s:4:"ba89";s:23:"pi/guest_template1.tmpl";s:4:"29f4";s:23:"pi/guest_template2.tmpl";s:4:"dedf";}',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+			'php' => '3.0.0-',
+			'typo3' => '3.5.0-',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
 );
 
 ?>

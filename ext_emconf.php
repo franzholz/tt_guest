970ac4e31ebf28ea3970051d60dc2af214959861
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-05-01 17:22:18 +0000

    Version 1.1.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tt_guest/trunk@2709 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "tt_guest"
 #
-# Auto generated 02-04-2006 10:02
+# Auto generated 01-05-2006 19:23
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'Freelancer',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '1.1.0',
-	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"c31b";s:28:"class.tx_ttguest_wizicon.php";s:4:"2847";s:21:"ext_conf_template.txt";s:4:"dbec";s:12:"ext_icon.gif";s:4:"6754";s:15:"ext_icon__h.gif";s:4:"e999";s:17:"ext_localconf.php";s:4:"7f93";s:14:"ext_tables.php";s:4:"a1ff";s:14:"ext_tables.sql";s:4:"515e";s:28:"ext_typoscript_constants.txt";s:4:"a432";s:24:"ext_typoscript_setup.txt";s:4:"1451";s:18:"flexform_ds_pi.xml";s:4:"d8f8";s:13:"guestbook.gif";s:4:"b513";s:13:"locallang.php";s:4:"7a32";s:25:"locallang_csh_ttguest.php";s:4:"644c";s:17:"locallang_tca.php";s:4:"a271";s:7:"tca.php";s:4:"9066";s:12:"doc/TODO.txt";s:4:"d602";s:14:"doc/manual.sxw";s:4:"7d25";s:23:"pi/class.tx_ttguest.php";s:4:"7447";s:39:"pi/class.tx_ttguest_RecordNavigator.php";s:4:"8a30";s:18:"pi/guest_help.tmpl";s:4:"a7e0";s:18:"pi/guest_help1.gif";s:4:"817b";s:19:"pi/guest_submit.inc";s:4:"ba89";s:23:"pi/guest_template1.tmpl";s:4:"29f4";s:23:"pi/guest_template2.tmpl";s:4:"dedf";}',
+	'version' => '1.1.1',
+	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"4350";s:28:"class.tx_ttguest_wizicon.php";s:4:"2847";s:21:"ext_conf_template.txt";s:4:"dbec";s:12:"ext_icon.gif";s:4:"6754";s:15:"ext_icon__h.gif";s:4:"e999";s:17:"ext_localconf.php";s:4:"7f93";s:14:"ext_tables.php";s:4:"a1ff";s:14:"ext_tables.sql";s:4:"515e";s:28:"ext_typoscript_constants.txt";s:4:"a432";s:24:"ext_typoscript_setup.txt";s:4:"4a15";s:18:"flexform_ds_pi.xml";s:4:"d8f8";s:13:"guestbook.gif";s:4:"b513";s:13:"locallang.php";s:4:"7a32";s:25:"locallang_csh_ttguest.php";s:4:"644c";s:17:"locallang_tca.php";s:4:"a271";s:7:"tca.php";s:4:"9066";s:12:"doc/TODO.txt";s:4:"d602";s:14:"doc/manual.sxw";s:4:"0301";s:23:"pi/class.tx_ttguest.php";s:4:"42d3";s:39:"pi/class.tx_ttguest_RecordNavigator.php";s:4:"3b6d";s:18:"pi/guest_help.tmpl";s:4:"ab19";s:18:"pi/guest_help1.gif";s:4:"817b";s:19:"pi/guest_submit.inc";s:4:"ba89";s:23:"pi/guest_template1.tmpl";s:4:"29f4";s:23:"pi/guest_template2.tmpl";s:4:"dedf";}',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
